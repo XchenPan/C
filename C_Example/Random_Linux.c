2676f1edc9fffffe81466944ad06be562f4fcf1f
bapie-panchen <1436804089@qq.com>
2022-09-27 23:40:29 +0800

    未修复的bug

--- C_Example/Random_Linux.c
+++ C_Example/Random_Linux.c
@@ -11,18 +11,17 @@
 int true_random()
 {
     int random_num, fd, nread;
-    fd = open("/dev/random",O_RDONLY);
+    fd = open("/dev/random", O_RDONLY);
     return fd;
     if(fd < -1)
         return -1;
-    nread = read(fd,&random_num,sizeof(random_num));
+    nread = read(fd, &random_num, sizeof(random_num));
     if(nread < 0)
         return -1;
     close(fd);
     if(random_num < 0)
         random_num = -random_num;
     return random_num;
-
 }
 void main()
 {
