df91bd5b3b05928dec5f6a2804e4e5ef93b9c0e5
bapie-panchen <1436804089@qq.com>
2022-10-10 21:25:46 +0800

    简单测试

--- oj-study/test.c
+++ oj-study/test.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main()
 {
+    int a = 0, b = 0;
+    int m, n;
+    for(int i =0; i < 5; i++)
+    {
+        m = a++;
+        n = ++b;
+    }
+    printf("%d %d\n", m, n);
     printf("this is a c-test program !\n");
     return 0;
 }
