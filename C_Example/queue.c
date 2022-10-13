0986bafc6669226b29b9b4ef6033cfaf4186e9cb
bapie-panchen <1436804089@qq.com>
2022-10-13 10:58:03 +0800

    简单优化

--- C_Example/queue.c
+++ C_Example/queue.c
@@ -1,40 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MaxSize 100
-struct queue
+struct Queue
 {
-    int *data;
+    int *Data;
     int head; //队头
     int tail; //队尾
     int capacity; //队列最大容量
 };
-struct queue *InitQueue(int size)
+struct Queue *InitQueue(int size)
 {
-    struct queue *q = (struct queue*)malloc(sizeof(struct queue));
-    q->data = (int*)malloc(sizeof(int)*size);
+    struct Queue *q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->Data = (int*)malloc(sizeof(int)*size);
     q->head = q->tail;
     q->capacity = size;
     return q;
 }
-void InputQueue(struct queue *q)
+void InputQueue(struct Queue *q)
 {
 		  for (int i = 0; i < 10 ; i++) {
-				q->data[i] = i;
+				q->Data[i] = i;
 				q->tail = (q->tail + 1) % q->capacity;
 		  }
 		  printf("\n");
 }
-void PrintQueue(struct queue *q)
+void PrintQueue(struct Queue *q)
 {
 		  for (int i = q->head; i != q->tail;) {
-				printf("%d ", q->data[i]);
+				printf("%d ", q->Data[i]);
 				i = (i + 1) % q->capacity;
 		  }
 		  printf("\n");
 }
 int main()
 {
-    struct queue *q = InitQueue(MaxSize);
+    struct Queue *q = InitQueue(MaxSize);
     InputQueue(q);
     PrintQueue(q);
     printf("hello world\n");
